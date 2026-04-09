0b366328e8bdcaa24b2fe0fdf1e82814d58b393a
alexr <alexramirez92606@gmail.com>
2026-04-09 15:55:46 -0500

    basic calculator

--- src/main/java/com/pluralsight/basicCalculator.java
+++ src/main/java/com/pluralsight/basicCalculator.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class basicCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("Possible calculations:");
+        System.out.println(" (A)dd");
+        System.out.println(" (S)ubtract");
+        System.out.println(" (M)ultiply");
+        System.out.println(" (D)ivide");
+
+        System.out.print("Please select an option: ");
+        char choice = input.next().charAt(0);
+
+        double result = num1 * num2;
+
+        System.out.println(num1 + " * " + num2 + " = " + result);
+
+    }
 }
